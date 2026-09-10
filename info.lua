add5b9dfb1024230580b75bbe7e9b56e86a3ae34
Orlando Suarez <orlando@local>
2026-09-10 09:35:12 +0000

    Bump version to 0.0.1 for the current pre-release/test build

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
     Name = "Monitor Audio~IA Series",
-    Version = "0.2.0",
-    BuildVersion = "0.2.0.0",
+    Version = "0.0.1",
+    BuildVersion = "0.0.1.0",
     Id = "8f3f3b8c-7e4d-4b16-9b55-7d2e9e4e8b21",
     Author = "Monitor Audio",
     Description = "Q-SYS plugin for Monitor Audio IA Series installation amplifiers."
